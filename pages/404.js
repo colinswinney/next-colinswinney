0e5f16caf5baa7697a4e4c1f45b3dbead7360c14
Colin Swinney <colinjswinney@gmail.com>
2021-09-26 22:57:48 -0500

    stupid apostrophe

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ const ErrorPage = () => {
         <Main>
             <Container>
                 <h1>Oh no, a 404!</h1>
-                <p>Sorry, this page doesn't exist, you should probably <Link to="/" passHref><a>head home</a></Link>.</p> 
+                <p>Sorry, this page doesn&apos;t exist, you should probably <Link to="/" passHref><a>head home</a></Link>.</p> 
             </Container>
         </Main>
     )
